631ba9640a3f47f6df3e99ade10e3e850ce78b16
EamaniManasa <147276480+EamaniManasa@users.noreply.github.com>
2023-10-10 23:56:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import GridExample2 from './GridExample2.js';
+
+const App = () => {
+  return (
+    <div>
+      <GridExample2 />
+    </div>
+  );
+};
+
+export default App;
